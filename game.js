0add175553d339cadf8096e608aab3464a593269
ameykadam <85875577+ameykadam@users.noreply.github.com>
2021-11-16 18:12:46 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,7 @@
+function addUser(){
+    player1_name=document.getElementById("player_1").value;
+    player2_name=document.getElementById("player_2").value;
+    localStorage.setItem("player1",player1_name);
+    localStorage.setItem("player2",player2_name);
+    window.Location="game.html";
+}
